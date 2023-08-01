badb167e5cc28504a9304af84757c5d5d223e135
Tri Hoang <trihoang09@gmail.com>
2023-08-01 15:18:44 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){  
+   const results = record.find(record => record.result === 'W')
+   if(!results){
+    return undefined
+   } else {
+    return results.year
+   }
+} 
